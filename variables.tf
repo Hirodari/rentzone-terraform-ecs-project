9c382c7efe1bc6c99ce2c59c2b529170c905f73d
Fred B <fredbitenyo@gmail.com>
2023-05-01 20:46:47 +0200

    adding alb, acm, rds, rentzone.env

--- variables.tf
+++ variables.tf
@@ -61,3 +61,42 @@ variable "private_data_subnet_az2" {
   description = "private data subnet az 2 cidr block"
   type        = string
 }
+
+# ssh variables
+variable "ssh_location" {
+  description = "ssh location from my ip location"
+  type        = string
+}
+
+# rds variables
+variable "db_snapshot_identifier" {
+  description = "database snapshot name"
+  type        = string
+}
+
+variable "db_instance_class" {
+  description = "database sinstance type"
+  type        = string
+}
+
+variable "db_instance_identifier" {
+  description = "database sinstance type"
+  type        = string
+}
+
+variable "multi_az_deployment" {
+  description = "create a standby db instance"
+  type        = bool
+}
+
+# acm variables
+
+variable "domain_name" {
+  description = "domain name"
+  type        = string
+}
+
+variable "alternative_names" {
+  description = "alternative domain names"
+  type        = string
+}
